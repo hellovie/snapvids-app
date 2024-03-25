21c4b41c830aff8b983d88d973d02a29bdca9f39
Hellovie <hellovie@foxmail.com>
2024-03-26 02:19:02 +0800

    feat(common): 新增设计变量和颜色配置

---
+++ lib/common/config/common_colors.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+class CommonColors {
+  /// 成功色
+  static const Color success_1 = Color(0xFF004D1C);
+  static const Color success_2 = Color(0xFF046625);
+  static const Color success_3 = Color(0xFF0A802D);
+  static const Color success_4 = Color(0xFF129A37);
+  static const Color success_5 = Color(0xFF1DB440);
+  static const Color success_6 = Color(0xFF27C346);
+  static const Color success_7 = Color(0xFF50D266);
+
+  /// 警示色
+  static const Color warning_1 = Color(0xFF4D1B00);
+  static const Color warning_2 = Color(0xFF793004);
+  static const Color warning_3 = Color(0xFFA64B0A);
+  static const Color warning_4 = Color(0xFFD26913);
+  static const Color warning_5 = Color(0xFFFF8D1F);
+  static const Color warning_6 = Color(0xFFFF9626);
+  static const Color warning_7 = Color(0xFFFFB357);
+
+  /// 错误色
+  static const Color danger_1 = Color(0xFF4D000A);
+  static const Color danger_2 = Color(0xFF770611);
+  static const Color danger_3 = Color(0xFFA1161F);
+  static const Color danger_4 = Color(0xFFCB2E34);
+  static const Color danger_5 = Color(0xFFF54E4E);
+  static const Color danger_6 = Color(0xFFF76965);
+  static const Color danger_7 = Color(0xFFF98D86);
+
+  /// 链接色
+  static const Color link_1 = Color(0xFF000D4D);
+  static const Color link_2 = Color(0xFF041B79);
+  static const Color link_3 = Color(0xFF0E32A6);
+  static const Color link_4 = Color(0xFF1D4DD2);
+  static const Color link_5 = Color(0xFF306FFF);
+  static const Color link_6 = Color(0xFF3C7EFF);
+  static const Color link_7 = Color(0xFF689FFF);
+}
---
+++ lib/common/config/design_variables.dart
@@ -0,0 +1,20 @@
+class DesignVariables {
+  /// 边框圆角
+  static const double borderRadiusNone = 0;
+  static const double borderRadiusSmall = 2;
+  static const double borderRadiusMedium = 4;
+  static const double borderRadiusLarge = 8;
+
+  /// 间距大小
+  static const double spaceNone = 0;
+  static const double spaceSmall = 4;
+  static const double spaceMedium = 8;
+  static const double spaceLarge = 16;
+  static const double spaceLarger = 20;
+
+  /// 边框大小
+  static const double borderNone = 0;
+  static const double borderSmall = 1;
+  static const double borderMedium = 2;
+  static const double borderLarge = 3;
+}
